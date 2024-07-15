45ddfb3e10122e50972e6e5960d636f04570e71a
Ning （Home） <ning@digisalad.cool>
2024-07-16 02:47:25 +0800

    hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import { getUserState } from '@/function/api'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/:pathMatch(.*)*', // router 4
