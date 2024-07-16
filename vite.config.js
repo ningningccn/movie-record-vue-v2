b212b860c52e2bf897e29e049e83ec1091e7ccfb
Ning <ning@digisalad.cool>
2024-07-16 12:39:54 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: 'https://ningningccn.github.io/movie-record-vue-v2/',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
