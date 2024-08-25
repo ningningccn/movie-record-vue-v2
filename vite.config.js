6c061b2561aae3e9a55a0a73b31bf29b4b1da698
Ning （Home） <ning@digisalad.cool>
2024-08-26 04:16:54 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/<REPO>/',
+    base: '/',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
